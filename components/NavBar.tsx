ac41975714b9f15cedbed4909c450daaaa4baf40
AneshSM <aneshsm204@gmail.com>
2023-08-09 18:30:37 +0530

    Updates

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,16 @@
+'use client'
 import { options } from "@app/api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
+import Image from "next/image";
 import Link from "next/link";
 
-export default async function NavBar() {
-  const session = await getServerSession(options);
+export default function NavBar() {
   return (
     <nav className="bg-slate-400 text-slate-50 p-4">
-      <ul className="flex justify-evenly text-1xl font-bold">
+      <Link href={'/'}>
+        <Image alt="logo" src={'/public/assets/logo/Interaction-logo.svg'} width={30} height={30} className="object-contain" />
+      </Link>
+      {/* <ul className="flex justify-evenly text-1xl font-bold">
         <li>
           <Link href="/">Home</Link>
         </li>
@@ -25,7 +29,7 @@ export default async function NavBar() {
               <Link href="/projects">Projects</Link>
             </li>
           )}
-      </ul>
+      </ul> */}
     </nav>
   );
 }
